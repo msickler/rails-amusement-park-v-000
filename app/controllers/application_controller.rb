6cdef238872dacd5ea73d181a42d02a5061851e2
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-14 18:48:09 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def current_user 
+  def current_user
     @user = User.find_by(id: session[:user_id]) || User.new
-  end 
+  end
 
   def logged_in?
-    current_user.id != nil 
-  end 
+    current_user.id != nil
+  end
 
 end
