9469b1a1adf5a9729eab0600ce26f0ac7957b7a5
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-14 18:28:35 +0000

    automatically backed up by learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,20 +1,20 @@
 class AttractionsController < ApplicationController
 
   def index
-    @attractions = Attractions.all 
+    @attractions = Attractions.all
   end
 
   def new
-    @attraction = Attraction.new 
+    @attraction = Attraction.new
   end
 
   def create
     @attraction = Attraction.new(attraction_params)
-    if @attraction.save 
+    if @attraction.save
       redirect_to attraction_path(@attraction)
-    else 
+    else
       redirect_to '/'
-    end    
+    end
   end
 
   def edit
@@ -25,15 +25,15 @@ class AttractionsController < ApplicationController
     @attraction = Attraction.find(params[:id])
     if @attraction = Attraction.update(params[:id], attraction_params)
       redirect_to attraction_path(@attraction)
-    else 
+    else
       redirect_to '/'
-    end 
+    end
   end
 
   private
 
   def attraction_params
-
+    params.require(:attraction).permit(:name, :tickets, :nausea_rating, :happiness_rating, :min_height)
   end
 
 end
