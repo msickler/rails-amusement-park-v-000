4ad36c5f35540a716f60ef6e2a8f14530fbc2e44
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-14 18:06:58 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,14 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if params[:user][:admin] == 1 
-      @user.admin = true 
+    if params[:user][:admin] == 1
+      @user.admin = true
     end
     if @user.save
       session[:id] = @user.id
       redirect_to user_path(@user)
     else
-      render :new 
+      render :new
     end
 
   end
