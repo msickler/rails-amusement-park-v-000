489fd8cc9639c70f74d2dfb48981a529ec6ca708
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-14 18:23:12 +0000

    automatically backed up by learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,3 +1,29 @@
 class AttractionsController < ApplicationController
 
+  def index 
+
+  end 
+
+  def new 
+
+  end 
+
+  def create 
+
+  end 
+
+  def edit 
+
+  end 
+
+  def update 
+
+  end 
+
+  private 
+
+  def attraction_params 
+
+  end 
+
 end 
