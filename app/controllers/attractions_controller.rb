b31942589fbb38eae672367936141f6fab2b0543
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-14 20:49:49 +0000

    automatically backed up by learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,52 +1,47 @@
 class AttractionsController < ApplicationController
   before_action :set_attraction, only: [:show, :edit, :update]
 
-    def index
-      @attractions = Attraction.all
-      @user = User.find(session[:user_id])
-    end
-
-    def show
-      @user = User.find(session[:user_id])
-    end
-
-    def new
-      @attraction = Attraction.new
-    end
-
-    def create
-      @attraction = Attraction.new(attraction_params)
-      if @attraction.save
-        flash[:notice] = "#{@attraction.name} was created successfuly!"
-        redirect_to "/attractions/#{@attraction.id}"
-      else
-        flash[:notice] = "Sorry, something went wrong. Could not create this attraction."
-        redirect_to "/attractions/new"
-      end
-    end
-
-    def edit
-    end
-
-    def update
-     if @attraction.update(attraction_params)
-         redirect_to "/attractions/#{@attraction.id}"
-     else
-        "/attractions/#{@attraction.id}/edit"
-     end
-   end
-
-
-
-    private
-
-    def set_attraction
-      @attraction = Attraction.find(params[:id])
-    end
-
-
-    def attraction_params
-       params.require(:attraction).permit(:name, :tickets, :nausea_rating, :happiness_rating, :min_height)
-    end
-
+  def index
+    @attractions = Attraction.all
+    @user = User.find(session[:user_id])
   end
+
+  def show
+    @user = User.find(session[:user_id])
+  end
+
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.new(attraction_params)
+    if @attraction.save
+      redirect_to "/attractions/#{@attraction.id}"
+    else
+      redirect_to "/attractions/new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @attraction.update(attraction_params)
+        redirect_to "/attractions/#{@attraction.id}"
+    else
+        "/attractions/#{@attraction.id}/edit"
+    end
+  end
+
+  private
+
+  def set_attraction
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :nausea_rating, :happiness_rating, :tickets, :min_height)
+  end
+
+end
