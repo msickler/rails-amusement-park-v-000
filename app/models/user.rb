5753e7c28b7520140e93a33fb616c8287c043a5e
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-14 18:03:00 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ActiveRecord::Base
   has_many :attractions, through: :rides
   has_secure_password
 
-  def mood 
-    if happiness.present?
-      happiness > nausea ? "happy" : "sad" 
-    end 
+  def mood
+    if happiness.present? && nausea.present? 
+      happiness > nausea ? "happy" : "sad"
+    end
   end
 end
