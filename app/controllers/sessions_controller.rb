e685484cc52665e326bd47d00660fe260d6f1906
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-14 20:16:43 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,24 @@
 class SessionsController < ApplicationController
-
-  def new
-    @user = User.new
+  
+    def new
+      @user = User.new
+    end
+  
+    def create
+      @user = User.find_by(name: params[:user][:name])
+  
+      if @user && @user.authenticate(params[:user][:password])
+        flash[:notice] = "#{@user.name} successfully logged in!"
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        flash[:notice] = "Something went wrong. Please try again!"
+        redirect_to new_session_path
+      end
+    end
+  
+    def destroy
+      session.delete :user_id
+      redirect_to '/'
+    end
   end
-
-  def create
-    @user = User.find_by(name: params[:user][:name])
-    if @user && @user.authenticate(params[:user][:password])
-       flash[:notice] = "#{@user.name} successfully logged in!"
-       session[:user_id] = @user.id
-       redirect_to user_path(@user)
-     else
-       flash[:notice] = "Something went wrong. Please try again!"
-       redirect_to new_session_path
-     end
-  end
-
-  def destroy
-    session.delete :user_id
-    redirect_to '/'
-  end
-
-
-end
