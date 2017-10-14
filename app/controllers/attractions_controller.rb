1c2553082a3ae3a760ac1562514ca0c84f45ce51
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-14 20:18:32 +0000

    automatically backed up by learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,54 +1,47 @@
 class AttractionsController < ApplicationController
   before_action :set_attraction, only: [:show, :edit, :update]
-
+  
     def index
       @attractions = Attraction.all
+      @user = User.find(session[:user_id])
     end
-
-  def new
-    @attraction = Attraction.new
-  end
-
-  def create
-    @attraction = Attraction.new(attraction_params)
-    if @attraction.save
-      redirect_to attraction_path(@attraction)
-    else
-      redirect_to "/"
+  
+    def show
+      @user = User.find(session[:user_id])
     end
-  end
-
-  def edit
-    @attraction = Attraction.find(params[:id])
-  end
-
-  def update
-    if @attraction = Attraction.update(params[:id], attraction_params)
-      redirect_to attraction_path(@attraction)
-    else
-      redirect_to "/"
+  
+    def new
+      @attraction = Attraction.new
     end
-  end
-
-  def show
-    @attraction = Attraction.find(params[:id])
-  end
-
-  def ride
-    @ride = Ride.new
-    @ride.attraction = Attraction.find(params[:id])
-    @ride.user = User.find(session[:user_id])
-    if flash[:message] = @ride.take_ride
-      redirect_to user_path(@ride.user)
-    else
-      redirect_to "/"
+  
+    def create
+      @attraction = Attraction.new(attraction_params)
+      if @attraction.save
+        redirect_to "/attractions/#{@attraction.id}"
+      else
+        redirect_to "/attractions/new"
+      end
     end
-
+  
+    def edit
+    end
+  
+    def update
+      if @attraction.update(attraction_params)
+          redirect_to "/attractions/#{@attraction.id}"
+      else
+          "/attractions/#{@attraction.id}/edit"
+      end
+    end
+  
+    private
+  
+    def set_attraction
+      @attraction = Attraction.find(params[:id])
+    end
+  
+    def attraction_params
+      params.require(:attraction).permit(:name, :nausea_rating, :happiness_rating, :tickets, :min_height)
+    end
+  
   end
-
-  private
-
-  def attraction_params
-    params.require(:attraction).permit(:name, :tickets, :nausea_rating, :happiness_rating, :min_height)
-  end
-end
