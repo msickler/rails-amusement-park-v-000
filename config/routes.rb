7a2d93a6be97cfa4d47a6e0edb1865788812f533
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-14 21:09:25 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
   resources :attractions
   resources :sessions, only:[:new, :show, :create]
   get '/signin' => 'sessions#new'
-  root 'welcome#index'
   post'/logout' => 'sessions#destroy', as: :logout
   resources :users do
     resources :attractions
