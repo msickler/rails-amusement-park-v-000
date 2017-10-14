14e52a8625e6d7f885c5569a5fc847fe0393acee
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-14 19:44:02 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:id] = @user.id
+      session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
       redirect_to new_user_path
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(id: params[:id])
   end
 
 
