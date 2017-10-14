9a5b09a583ef52bf576abac8afdfca0dcebd7908
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-14 17:44:46 +0000

    automatically backed up by learn

--- db/migrate/20140703140419_create_rides.rb
+++ db/migrate/20140703140419_create_rides.rb
@@ -1,12 +1,12 @@
 class CreateRides < ActiveRecord::Migration
-  def change 
-    create_table :rides do |t| 
-      t.string :name 
-      t.integer :tickets 
-      t.integer :nausea_rating 
-      t.integer :happiness_rating 
+  def change
+    create_table :rides do |t|
+      t.string :name
+      t.integer :tickets
+      t.integer :nausea_rating
+      t.integer :happiness_rating
       t.integer :min_height
-    end 
-      
+    end
+
     end
 end
