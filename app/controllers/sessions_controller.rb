5b9aa030e4c0b3323cead8919b770a33b6a56ea7
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-14 18:12:59 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,25 @@
-class SessionsController < ApplicationController 
+class SessionsController < ApplicationController
 
-  def new 
-    @user = User.new 
+  def new
+    @user = User.new
   end
 
-  def create 
-    @user = User.new 
+  def create
+    @user = User.new
     if @user && @user.authenticate(params[:user][:password])
       flash[:notice] = "#{@user.name} has logged in."
-      session[:user_id] = @user.id 
+      session[:user_id] = @user.id
       redirect_to user_path(@user)
-    else 
+    else
       flash[:notice] = "Something went wrong. Please try again!"
       redirect_to 'new_session_path'
-    end 
-  end 
+    end
+  end
 
   def destroy
-    session[:user_id].delete 
+    session[:user_id].delete
     redirect_to '/'
-  end 
-    
+  end
+
 
 end 
