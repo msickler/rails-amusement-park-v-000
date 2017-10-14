fc003689de6b64a73b76f4551468153d880aaa27
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-14 17:54:46 +0000

    automatically backed up by learn

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController 
+
+  def new 
+    @user = User.new
+  end 
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save 
+      session[:id] = @user.id
+      redirect_to user_path(@user)
+    else 
+      redirect_to new_user_path 
+    end 
+    
+  end 
+
+  def show 
+
+  end 
+
+  private 
+
+  def user_params 
+  end 
+
+end 
