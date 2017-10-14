7b5dee3dbd07d78833e2cf2909c5ccbc7d5d6024
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-14 20:28:40 +0000

    automatically backed up by learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,17 +3,17 @@ class Ride < ActiveRecord::Base
   belongs_to :attraction
 
   def take_ride
-    if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height
-      "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
-    elsif  self.user.tickets < self.attraction.tickets
-      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
-    elsif self.user.height < self.attraction.min_height
-      "Sorry. You are not tall enough to ride the #{attraction.name}."
-    else
-      self.user.tickets = (self.user.tickets - self.attraction.tickets)
-      self.user.nausea = (self.user.nausea + self.attraction.nausea_rating)
-      self.user.happiness = (self.user.happiness + self.attraction.happiness_rating)
-      user.save
+    if self.user.tickets < self.attraction.tickets && self.attraction.min_height > self.user.height
+        "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+      elsif self.user.tickets < self.attraction.tickets
+        "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+      elsif self.attraction.min_height > self.user.height
+        "Sorry. You are not tall enough to ride the #{attraction.name}."
+      else
+        self.user.tickets = (self.user.tickets - self.attraction.tickets)
+        self.user.nausea = (self.user.nausea + self.attraction.nausea_rating)
+        self.user.happiness = (self.user.happiness + self.attraction.happiness_rating)
+        user.save
     end
   end
 end
