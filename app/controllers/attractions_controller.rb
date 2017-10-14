54821b868addf9ef588f4d62532b11c7249c5f97
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-14 20:18:36 +0000

    automatically backed up by learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,19 +1,19 @@
 class AttractionsController < ApplicationController
   before_action :set_attraction, only: [:show, :edit, :update]
-  
+
     def index
       @attractions = Attraction.all
       @user = User.find(session[:user_id])
     end
-  
+
     def show
       @user = User.find(session[:user_id])
     end
-  
+
     def new
       @attraction = Attraction.new
     end
-  
+
     def create
       @attraction = Attraction.new(attraction_params)
       if @attraction.save
@@ -22,10 +22,10 @@ class AttractionsController < ApplicationController
         redirect_to "/attractions/new"
       end
     end
-  
+
     def edit
     end
-  
+
     def update
       if @attraction.update(attraction_params)
           redirect_to "/attractions/#{@attraction.id}"
@@ -33,15 +33,15 @@ class AttractionsController < ApplicationController
           "/attractions/#{@attraction.id}/edit"
       end
     end
-  
+
     private
-  
+
     def set_attraction
       @attraction = Attraction.find(params[:id])
     end
-  
+
     def attraction_params
       params.require(:attraction).permit(:name, :nausea_rating, :happiness_rating, :tickets, :min_height)
     end
-  
+
   end
